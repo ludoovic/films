3a5c4373a84d28b14d08679c75e98260ec6b4289
ludo <ludoovic@fre.fr>
2019-07-12 16:24:56 +0200

    oulala

---
+++ films.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 15:36:09 2019
+
+@author: AELION
+"""
+
+print("J'aime les films !")
---
+++ serietv.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 15:18:33 2019
+
+@author: AELION
+"""
+
+print("J'aime serietv")
